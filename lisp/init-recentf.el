c7091e76b0afd0f452bd57491eb85d0e73379744
lipb <lipb_2021@163.com>
2022-02-07 22:23:44 +0800

    set key open recentf file -- C-x C-r

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -7,6 +7,7 @@
  recentf-max-saved-items 1000
  recentf-exclude `("/tmp/" "/ssh:" ,(concat package-user-dir "/.*-autoloads\\.el\\'")))
 
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
 (provide 'init-recentf)
 ;;; init-recentf.el ends here
